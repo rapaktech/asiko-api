dc6aabc61ddd4d7bf2c6161b9b34c73dbe67edf7
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-02-03 14:57:27 +0100

    bug fix

--- src/middleware/user.js
+++ src/middleware/user.js
@@ -4,7 +4,7 @@ const { decodeToken } = require('./../services/jwt');
 module.exports = () => {
     return async (req, res, next) => {
         try {
-            const token = req.body.token;
+            const token = req.headers.authorization;
             if (!token) return res.status(400).json({ message: "Token Missing. Please Sign In Again To Access This Page. "});
 
             const decodedToken = decodeToken(token);
